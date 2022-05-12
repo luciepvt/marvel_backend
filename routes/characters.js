8bc2b17135b234853756ff437b60eda29beb33d9
Lucie Pivoteau <pivoteau.lucie@gmail.com>
2022-05-12 23:51:26 +0200

    page

--- routes/characters.js
+++ routes/characters.js
@@ -4,10 +4,9 @@ const axios = require("axios");
 
 router.get("/characters", async (req, res) => {
   try {
-    const page = req.query.page ? Number(req.query.page) : 1;
     const skip = (req.query.page - 1) * 100;
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&page=${page}&skip=${skip}&name=${req.query.search}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&skip=${skip}&name=${req.query.search}`
     );
     console.log(response.data);
     res.status(200).json(response.data);
--- routes/comics.js
+++ routes/comics.js
@@ -4,10 +4,9 @@ const axios = require("axios");
 
 router.get("/comics", async (req, res) => {
   try {
-    const page = req.query.page ? Number(req.query.page) : 1;
     const skip = (req.query.page - 1) * 100;
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&page=${page}&skip=${skip}&title=${req.query.search}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&skip=${skip}&title=${req.query.search}`
     );
     console.log(response.data);
     res.status(200).json(response.data);
